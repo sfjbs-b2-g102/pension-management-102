4644f4386b18004216a23841277b2d58af5d148f
sfjbs-b2-g102 <sfjbsolutionsb1g102@gmail.com>
2022-06-23 09:36:33 +0000

    Eureka and pensionDetail-Lokesh,Tangadorai and Authorization and APiGateWay-Sindhu,Kavya

---
+++ PensionAuthorization/src/main/java/com/jwt/api/SpringSecurityJwtExampleApplication.java
@@ -0,0 +1,35 @@
+package com.jwt.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.jwt.api.entity.User;
+import com.jwt.api.repository.UserRepository;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class SpringSecurityJwtExampleApplication {
+    @Autowired
+    private UserRepository repository;
+
+    @PostConstruct
+    public void initUsers() {
+        List<User> users = Stream.of(
+                new User(101, "lokesh", "password1", "lokesh@gmail.com"),
+                new User(102, "tangadorai", "password2", "tangadorai@gmail.com"),
+                new User(103, "sindhu", "password3", "sindhu@gmail.com"),
+                new User(104, "kavya", "password4", "kavya@gmail.com")
+        ).collect(Collectors.toList());
+        repository.saveAll(users);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtExampleApplication.class, args);
+    }
+
+}
